2d2ee8c31119011cb344b57d79e359c3cca503a9
Ian McCarthy <ian.mccarthy@emory.edu>
2023-02-27 08:56:25 -0500

    stuff

--- data-code/TaxBurden_Data.R
+++ data-code/TaxBurden_Data.R
@@ -13,9 +13,8 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
 
 
-source('data-code/paths.R')
-cig.data <- read_csv(paste0(path.data,"CDC_1970-2018.csv"), col_names = TRUE)
-cpi.data <- read_xlsx(paste0(path.bls,"CPI_1913_2019.xlsx"), skip = 11)
+cig.data <- read_csv("data/input/CDC_1970-2018.csv", col_names = TRUE)
+cpi.data <- read_xlsx("data/input/CPI_1913_2019.xlsx", skip = 11)
 
 
 # Clean tobacco data --------------------------------------------------------------
@@ -59,7 +58,7 @@ final.data <- final.data %>%
   left_join(cpi.data, by="Year") %>%
   mutate(price_cpi=cost_per_pack*(218/index))
 
-write_tsv(final.data,path=paste0(path.final,"data/TaxBurden_Data.txt"),append=FALSE,col_names=TRUE)
-write_rds(final.data,paste0(path.final,"data/TaxBurden_Data.rds"))
+write_tsv(final.data,"data/output/TaxBurden_Data.txt",append=FALSE,col_names=TRUE)
+write_rds(final.data,"data/output/TaxBurden_Data.rds")
 
 
